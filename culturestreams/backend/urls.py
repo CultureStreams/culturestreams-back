361e0106d430c37acd081d30501cd17ababa1057
BlitzundBogen <19719979+BlitzundBogen@users.noreply.github.com>
2020-03-28 04:51:29 +0100

    api-token-auth path gelöscht

--- culturestreams/backend/urls.py
+++ culturestreams/backend/urls.py
@@ -6,5 +6,5 @@ from rest_framework.authtoken import views
 
 urlpatterns = [
     path('api/v0/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth')
+    #path('api-token-auth/', views.obtain_auth_token, name='api-token-auth')
 ]
