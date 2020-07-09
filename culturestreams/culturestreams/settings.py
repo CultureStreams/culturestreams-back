1fb0b59a6757868ffd2b027d134b943a10443877
BlitzundBogen <19719979+BlitzundBogen@users.noreply.github.com>
2020-07-09 23:29:43 +0200

    now

--- culturestreams/culturestreams/settings.py
+++ culturestreams/culturestreams/settings.py
@@ -89,7 +89,7 @@ CORS_ORIGIN_REGEX_WHITELIST = [
     r"^https://\w+\.culturestreams\.org$",
     "http://localhost:4200",
     "http://localhost:8080",
-    "https://carinaland.github.io"
+    "https://carinaland.github.io",
 ]
 #localhost TODO
 
